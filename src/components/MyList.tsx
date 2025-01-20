1819c9a65df804e62f4232a77ac4719887579266
liasouris <lia.souris@student.lut.fi>
2025-01-20 13:45:54 +0200

    fixed issue with MyList

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -13,25 +13,26 @@ interface MyListProps {
 }
 
 const MyList: React.FC<MyListProps> = ({ header, items, updateList }) => {
-  return (
-    <div>
-      <h2>{header}</h2>
-      <ol>
-        {items.map((item) => (
-          <li
-            key={item.id}
-            onClick={() => updateList(item.id)}
-            style={{
-              textDecoration: item.clicked ? 'line-through' : 'none',
-              cursor: 'pointer',
-            }}
-          >
-            {item.text}
-          </li>
-        ))}
-      </ol>
-    </div>
-  );
-};
-
-export default MyList;
+    return (
+      <div>
+        <h2>{header}</h2>
+        <ol>
+          {items.map((item) => (
+            <li
+              key={item.id}
+              onClick={() => updateList(item.id)}
+              style={{
+                ...(item.clicked && { textDecoration: 'line-through' }),
+                cursor: 'pointer',
+              }}
+            >
+              {item.text}
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  };
+  
+  export default MyList;
+  
